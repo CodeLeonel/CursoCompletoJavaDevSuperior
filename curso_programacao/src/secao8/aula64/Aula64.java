a6ac55753892dca96c2ab22c5eefea1c28b11714
Hugo Leonel <hugoleonel.code@gmail.com>
2023-07-16 20:19:32 -0300

    Secao 8 Aula 64

---
+++ curso_programacao/src/secao8/aula64/Aula64.java
@@ -0,0 +1,49 @@
+package secao8.aula64;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import secao8.aula64.entities.Triangle;
+
+public class Aula64 {
+
+	public static void main(String[] args) {
+	
+		Locale.setDefault(Locale.US);
+		Scanner scan = new Scanner(System.in);
+		
+		Triangle x, y;
+		
+		x = new Triangle();
+		y = new Triangle();
+		
+		System.out.println("Enter the measures of triangle X: ");
+		x.a = scan.nextDouble();
+		x.b = scan.nextDouble();
+		x.c = scan.nextDouble();
+		
+		System.out.println("Enter the measures of triangle Y: ");
+		y.a = scan.nextDouble();
+		y.b = scan.nextDouble();
+		y.c = scan.nextDouble();
+		
+		double p = (x.a + x.b + x.c) / 2.0;
+		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+
+		p = (y.a + y.b + y.c) / 2.0;
+		double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+		
+		System.out.printf("Triangle X area: %.4f%n", areaX);
+		System.out.printf("Triangle X area: %.4f%n", areaY);
+		
+		if(areaX > areaY) {
+			System.out.println("Larger area: X");
+		} else {
+			System.out.println("Larger area: Y");			
+		}
+			
+		scan.close();
+		
+	}
+	
+}
---
+++ curso_programacao/src/secao8/aula64/entities/Triangle.java
@@ -0,0 +1,9 @@
+package secao8.aula64.entities;
+
+public class Triangle {
+
+	public double a;
+	public double b;
+	public double c;
+	
+}
