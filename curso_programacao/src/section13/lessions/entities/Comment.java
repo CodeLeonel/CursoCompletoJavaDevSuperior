1f55ac069b8cb4baaf1b4e4261a4fc5067972c6b
Hugo Leonel <hugoleonel.code@gmail.com>
2023-07-22 18:32:23 -0300

    Section 13 Lession 151

---
+++ curso_programacao/src/section13/lessions/entities/Comment.java
@@ -0,0 +1,15 @@
+package section13.lessions.entities;
+
+public class Comment {
+
+	private String text;
+
+	public Comment(String text) {
+		this.text = text;
+	}
+
+    public String toString() {
+    	return text;
+    }
+	
+}
