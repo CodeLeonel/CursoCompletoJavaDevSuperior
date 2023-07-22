7fde5989cbaeff0ee49c6e159134972b86fde4c4
Hugo Leonel <hugoleonel.code@gmail.com>
2023-07-22 07:12:49 -0300

    Section 13 Lession 146

---
+++ curso_programacao/src/section13/entities/enums/OrderStatus.java
@@ -0,0 +1,10 @@
+package section13.entities.enums;
+
+public enum OrderStatus {
+
+	PENDING_PAYMENT,
+	PROCESSING,
+	SHIPPED,
+	DELIVERED
+	
+}
--- curso_programacao/src/section13/lessions/Lession146.java
+++ curso_programacao/src/section13/lessions/Lession146.java
@@ -1,9 +1,28 @@
 package section13.lessions;
 
+import java.util.Date;
+
+import section13.entities.Order;
+import section13.entities.enums.OrderStatus;
+
 public class Lession146 {
 
 	public static void main(String[] args) {
 
+		Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+		
+		System.out.println(order);
+		
+		OrderStatus os1 = OrderStatus.DELIVERED;
+		
+		OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+		
+		System.out.println(os1);
+		
+		System.out.println(os2);
+		
+		
+		
 	}
 
 }
