4348c768bf162fffe6eff514f89c5008a656c205
Hugo Leonel <hugoleonel.code@gmail.com>
2023-07-23 14:44:25 -0300

    Section 14 Lession 163

--- curso_programacao/src/section14/lessions/Lession158.java
+++ curso_programacao/src/section14/lessions/Lession158.java
@@ -8,9 +8,9 @@ public class Lession158 {
 
 	public static void main(String[] args) {
 
-		Account acc1 = new Account(1001,"Alex",1000.0);
-		acc1.withdraw(200);
-		System.out.println(acc1.getBalance());
+		//Account acc1 = new Account(1001,"Alex",1000.0);
+		//acc1.withdraw(200);
+		//System.out.println(acc1.getBalance());
 		
 		Account acc2 = new SavingsAccount(1002, "Maria", 1000.00, 0.01);
 		acc2.withdraw(200);
