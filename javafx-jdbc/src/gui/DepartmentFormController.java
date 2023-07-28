c83b68377c68327b59e4312f12272969706d1b6d
Hugo Leonel <hugoleonel.code@gmail.com>
2023-07-28 19:55:21 -0300

    Section 25 Lession 383 - Observer design pattern to update tableview

--- javafx-jdbc/src/gui/DepartmentFormController.java
+++ javafx-jdbc/src/gui/DepartmentFormController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
@@ -23,6 +26,8 @@ public class DepartmentFormController implements Initializable {
 	
 	private DepartmentService service;
 	
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+	
 	@FXML
 	private TextField txtId;
 	
@@ -38,6 +43,11 @@ public class DepartmentFormController implements Initializable {
 	@FXML
 	private Button btCancel;
 	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+	
 	@FXML
 	public void onBtSaveAction(ActionEvent event) {
 		if(entity == null) {
@@ -49,19 +59,18 @@ public class DepartmentFormController implements Initializable {
         try {
         	entity = getFormData();
         	service.saveOrUpdate(entity);
+        	notifyDataChangeListeners();
         	Utils.currentStage(event).close();
         } catch(DbException e) {
         	Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
         }
 
 	}
-
-	private Department getFormData() {
-		Department obj = new Department();
-		
-		obj.setId(Utils.tryParseToInt(txtId.getText()));
-		obj.setName(txtName.getText());
-		return obj;
+	
+	private void notifyDataChangeListeners() {
+		for(DataChangeListener listener : dataChangeListeners) {
+			listener.onDataChanged();
+		}
 	}
 
 	@FXML
@@ -69,22 +78,8 @@ public class DepartmentFormController implements Initializable {
 		Utils.currentStage(event).close();	
 	}
 	
-	@Override
-	public void initialize(URL url, ResourceBundle rb) {
-		initializeNodes();
-	}
-	
-	private void initializeNodes() {
-		Constraints.setTextFieldInteger(txtId);
-		Constraints.setTextFieldMaxLenght(txtName, 30);
-	}
-
-	public void setDepartment(Department entity) {
-		this.entity = entity;
-	}
-	
-	public void setDepartmentService(DepartmentService service) {
-		this.service = service;
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
 	}
 	
 	public void updateFormData() {
@@ -93,6 +88,27 @@ public class DepartmentFormController implements Initializable {
 		}
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
+	}
+
+	private void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLenght(txtName, 30);
+	}
+	
+	private Department getFormData() {
+		Department obj = new Department();
+		
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+		return obj;
+	}
+	
+	public void setDepartment(Department entity) {
+		this.entity = entity;
+	}
+	
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 	
 }
