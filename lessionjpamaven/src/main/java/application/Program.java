3389ef803bb4e6ba5a75a9045fe4f7f8e9f06f84
Hugo Leonel <hugoleonel.code@gmail.com>
2023-08-06 01:19:01 -0300

    Section 22 Lession 294B - Select person

--- lessionjpamaven/src/main/java/application/Program.java
+++ lessionjpamaven/src/main/java/application/Program.java
@@ -10,24 +10,19 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Person p1 = new Person(null, "Carlos da Silva", "carlos@gmail.com");
-		Person p2 = new Person(null, "Joaquim Torres", "joaquim@gmail.com");
-		Person p3 = new Person(null, "Ana Maria", "ana@gmail.com");
+		
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("sample-jpa");
 		EntityManager em = emf.createEntityManager();
 		
-		em.getTransaction().begin();
 		
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		
-		em.getTransaction().commit();
+		Person p = em.find(Person.class, 2);
+
+				
 		em.close();
 		emf.close();
 		
-		System.out.println("Ready!");
+		System.out.println(p);
 		
 		
 	}
