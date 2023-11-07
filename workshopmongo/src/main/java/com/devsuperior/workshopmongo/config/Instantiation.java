c494ab5f815b966212263f56cbd5d9ebcfc3b52b
Hugo Leonel <hugoleonel.code@gmail.com>
2023-11-06 23:10:30 -0300

    Section 24 Lession 339 - Database instantiation operation

---
+++ workshopmongo/src/main/java/com/devsuperior/workshopmongo/config/Instantiation.java
@@ -0,0 +1,33 @@
+package com.devsuperior.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.devsuperior.workshopmongo.domain.User;
+import com.devsuperior.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+
+		this.userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		this.userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		
+	}
+
+	
+	
+}
