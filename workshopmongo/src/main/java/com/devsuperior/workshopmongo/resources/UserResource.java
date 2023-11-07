b27755b4c3f2eeac2451fcb8bdb9873f7e41f66f
Hugo Leonel <hugoleonel.code@gmail.com>
2023-11-07 10:24:17 -0300

    Section 24 Lession 341 - Getting a user by id

--- workshopmongo/src/main/java/com/devsuperior/workshopmongo/resources/UserResource.java
+++ workshopmongo/src/main/java/com/devsuperior/workshopmongo/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,11 +20,19 @@ public class UserResource {
 	private UserService userService;
 
 	@GetMapping
-	public ResponseEntity<List<UserDTO>> getAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		
 		List<UserDTO> list = this.userService.findAll();
 		return ResponseEntity.ok().body(list);
 
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		
+		UserDTO user = this.userService.findById(id);
+		return ResponseEntity.ok().body(user);
+		
 	}
 	
 }
--- workshopmongo/src/main/java/com/devsuperior/workshopmongo/services/UserService.java
+++ workshopmongo/src/main/java/com/devsuperior/workshopmongo/services/UserService.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.workshopmongo.domain.User;
 import com.devsuperior.workshopmongo.dto.UserDTO;
 import com.devsuperior.workshopmongo.repository.UserRepository;
+import com.devsuperior.workshopmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,6 +19,15 @@ public class UserService {
 	public List<UserDTO> findAll() {
 		
 		return this.userRepository.findAll().stream().map(u -> new UserDTO(u)).toList();
+		
+	}
+	
+	public UserDTO findById(String id) {
+		
+		User user = this.userRepository.findById(id)
+							.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado. Id: " + id));
+		
+		return new UserDTO(user);
 		
 	}
 	
