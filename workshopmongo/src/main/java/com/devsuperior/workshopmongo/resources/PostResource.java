29f270cccb82ccedafe47e9050f3ac737e1e7775
Hugo Leonel <hugoleonel.code@gmail.com>
2023-11-12 09:13:02 -0300

    Section 24 Lession 354 - Query with multiple criteria

--- workshopmongo/src/main/java/com/devsuperior/workshopmongo/resources/PostResource.java
+++ workshopmongo/src/main/java/com/devsuperior/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.devsuperior.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +35,21 @@ public class PostResource {
 		
 		text = URL.decodeParam(text);
 		List<Post> posts = this.postService.findByTitle(text);
+		return ResponseEntity.ok().body(posts);
+		
+	}
+	
+	@GetMapping(value = "/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value="text", defaultValue="") String text, 
+			@RequestParam(value="minDate", defaultValue="") String minDate,
+			@RequestParam(value="maxDate", defaultValue="") String maxDate) {
+		
+		text = URL.decodeParam(text);
+		Date min = URL.converDate(minDate, new Date(0L));
+		Date max = URL.converDate(maxDate, new Date());
+		
+		List<Post> posts = this.postService.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(posts);
 		
 	}
--- workshopmongo/src/main/java/com/devsuperior/workshopmongo/services/PostService.java
+++ workshopmongo/src/main/java/com/devsuperior/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,11 @@ public class PostService {
 		
 		return this.postRepository.searchTitle(text);
 	
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return this.postRepository.fullSearch(text, minDate, maxDate);
 	}
 	
 }
