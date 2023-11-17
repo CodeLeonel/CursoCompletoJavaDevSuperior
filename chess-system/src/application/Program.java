cf1601a79c268f64123186ab0a5285c064aec5c5
Hugo Leonel <hugoleonel.code@gmail.com>
2023-11-17 10:43:29 -0300

    Section 16 Lession 195 - Switching player each turn

--- chess-system/src/application/Program.java
+++ chess-system/src/application/Program.java
@@ -18,7 +18,7 @@ public class Program {
 		while (true) {
 			try {
 				UI.clearScreen();
-				UI.printBoard(chess.getChessPieces());
+				UI.printMatch(chess);
 				System.out.println();
 				System.out.print("Source: ");
 
--- chess-system/src/chess/ChessMatch.java
+++ chess-system/src/chess/ChessMatch.java
@@ -8,11 +8,22 @@ import chess.pieces.Rook;
 
 public class ChessMatch {
 
+	private int turn;
+	
+	private Color currentPlayer;
+	
 	private Board board;
 
 	public ChessMatch() {
 		this.board = new Board(8, 8);
+		this.turn = 1;
+		this.currentPlayer = Color.WHITE;
 		initialSetup();
+	}
+	
+	private void nextTurn() {
+		turn++;
+		currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
 	}
 	
 	public boolean[][] possibleMoves(ChessPosition sourcePosition) {
@@ -27,6 +38,7 @@ public class ChessMatch {
 		validateSourcePosition(source);
 		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
+		nextTurn();
 		return (ChessPiece) capturedPiece;
 	}
 
@@ -40,6 +52,9 @@ public class ChessMatch {
 	private void validateSourcePosition(Position source) {
 		if(!board.thereIsAPiece(source)) {
 			throw new ChessException("There is no piece on source position");
+		}
+		if(currentPlayer != ((ChessPiece) board.getPiece(source)).getColor()) {
+			throw new ChessException("The chosen piece is not yours");
 		}
 		if(!board.getPiece(source).isThereAnyPossibleMove()) {
 			throw new ChessException("There is no possible moves for the chosen piece");
@@ -85,6 +100,12 @@ public class ChessMatch {
 		return mat;
 	}
 
+	public int getTurn() {
+		return turn;
+	}
 	
+	public Color getCurrentPlayer() {
+		return currentPlayer;
+	}
 
 }
